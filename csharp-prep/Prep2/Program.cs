d6a9584afebe4a6e704b6196dbf982d0323dd7e0
drewsteele408 <drewsteele408@gmail.com>
2025-04-25 17:18:07 -0600

    Prep 2 finished

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Runtime.InteropServices.Marshalling;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        string classGrade = "";
+
+        if (grade >= 90)
+        {
+            classGrade = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            classGrade = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            classGrade = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            classGrade = "D";
+        }
+
+        else 
+        {
+            classGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is {classGrade}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you pass!");
+        }
+
+        else
+        {
+            Console.WriteLine("Sorry you fail.");
+        }
+
     }
 }
